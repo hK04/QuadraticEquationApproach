2c1a9f4726dc7e648073ec466f47e871a00e9eb6
Matthew <matvey.anoshin@inbox.ru>
2022-08-21 13:19:32 +0300

    Add files via upload

--- headers.hpp
+++ headers.hpp
@@ -1,8 +1,7 @@
 /// \file
 
-
 ///This variable is the threshold of equality
-const double THRESHOLD = 1e-7; //delta neighbourhood of equality
+const double THRESHOLD = 1e-7;
 
 /** \struct roots
 *   @brief  Structure with varible `num_of_roots` and array of solutions
@@ -16,13 +15,13 @@ struct roots{
 
 /** \enum ExitStatus 
 *   @brief  Enum meant to treat different types of exits in solution
-*
+*   
 */
 
 enum ExitStatus{
-   INFINITE_SOLS = -1, ///  This is ExitStatus in case of Infinite number of Solutions
-   ZERO_SOLS     =  0, ///  This is ExitStatus in case of Zero number of Solutions
-   ONE_SOLS      =  1, ///  This is ExitStatus in case of One Solution
-   TWO_SOLS      =  2  ///  This is ExitStatus in case of Two Solutions
+   INFINITE_SOLS = -1, ///<This is ExitStatus in case of Infinite number of Solutions
+   ZERO_SOLS     =  0, ///<This is ExitStatus in case of Zero number of Solutions
+   ONE_SOLS      =  1, ///<This is ExitStatus in case of One Solution
+   TWO_SOLS      =  2  ///<This is ExitStatus in case of Two Solutions
 };
 
